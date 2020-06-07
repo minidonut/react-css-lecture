b889746722efbcfac81a112d2ee9130d8385c31e
minidonut <nycom13@gmail.com>
2020-06-07 12:27:25 +0900

    Add instagram reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { facebookReducer } from "./facebookReducer";
 import { watchaReducer } from "./watchaReducer";
+import { instagramReducer } from "./instagramReducer";
 
 
 export default combineReducers({
   facebook: facebookReducer,
   watcha: watchaReducer,
+  instagram: instagramReducer,
 });
---
+++ src/redux/reducers/instagramReducer.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = {
+  user: {
+    name: "정세훈",
+  }
+};
+
+export const instagramReducer = (state = INITIAL_STATE, { type, payload }) => {
+  return state;
+};
